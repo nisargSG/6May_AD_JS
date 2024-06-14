6fbcb0e8a05a185697efcce1cfd98226c036e10c
RinkalADesara <rinkaladesara.tops@gmail.com>
2024-06-14 16:03:18 +0530

    Done

--- Admin/dist/js/cat.js
+++ Admin/dist/js/cat.js
@@ -1,15 +1,17 @@
 let data = [];
-
+let editingId=null
 const dispCat = ()=>{
     let allData = JSON.parse(localStorage.getItem('catInfo'))
-    let tr = '';
+    
+let tr = '';
     if(allData != null){
         allData.map((i)=>{
             tr += `
                 <tr>
                 <td>${i.id}</td>
                 <td>${i.name}</td>
-                <td>Delete</td>
+                <td><button onclick='deleteData(${i.id})'>Delete</button>
+                <button onclick='editData(${i.id})'>Edit</button></td>
                 </tr>
             `
         })
@@ -24,12 +26,42 @@ const saveData = ()=>{
     let cname = document.catfrm.catname.value;
     let len = (getData !== null)?(getData.length+1):1;
     let info = {
-        "id":len,
+        "id":editingId ? editingId :((getData!==null)?(getData.length + 1 ):1),
         "name":cname
-    }   
-    data.push(info)
+    }
+    data=getData!==null?getData:[];
+    if(editingId){
+        data=data.map(cat => cat.id === editingId ? info: cat)
+        editingId=null
+    }
+    else{
+        data.push(info)
+    }
     localStorage.setItem('catInfo',JSON.stringify(data))
     document.catfrm.catname.value = '';
     dispCat()
     // document.catfrm.reset();
+
+}
+
+const deleteData = (id)=>{
+    let data1 = JSON.parse(localStorage.getItem('catInfo'))
+    data1.splice(id-1,1);
+    console.log(data1);
+    let j=1;
+    data1 = data1.map((i)=>{
+            i.id = j++;
+            return i;
+    })
+    data = data1;
+    localStorage.setItem('catInfo',JSON.stringify(data))
+    dispCat()
+}
+const editData= (id)=>{
+    let data2 = JSON.parse(localStorage.getItem('catInfo'))
+    let cat = data2.find(cat => cat.id===id)
+    if (cat){
+        document.catfrm.catname.value=cat.name;
+        editingId=id
+    }
 }
