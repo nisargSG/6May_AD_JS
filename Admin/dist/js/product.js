a73fa427723b80a13f949a09f9066ff27fce0d47
RinkalADesara <rinkaladesara.tops@gmail.com>
2024-06-21 16:21:47 +0530

    product update,delete

--- Admin/dist/js/product.js
+++ Admin/dist/js/product.js
@@ -17,7 +17,8 @@ const dispData = ()=>{
     let data = JSON.parse(localStorage.getItem('productInfo'));
     let catdata = JSON.parse(localStorage.getItem('catInfo'));
     let tr = '';
-    if(data !== ''){
+    console.log(data);
+    if(data !== null){
         data.map((i)=>{
             catdata.map((j)=>{
                 if(j.id == i.category){
@@ -31,7 +32,10 @@ const dispData = ()=>{
                 <td>${i.pname}</td>
                 <td>${i.category}</td>
                 <td>${i.price}</td>
-                <td>Delete</td>
+                <td>
+                    <button onclick="editData(${i.pid})">Edit</button>
+                    <button onclick='deleteData(${i.pid})'>Delete</button>
+                </td>
             </tr>
         `;
         })
@@ -57,6 +61,7 @@ const getImageUrl = (e)=>{
 }
 
 const saveData = ()=>{
+    let pid = document.prfrm.pid.value;
     let pname = document.prfrm.prname.value;
     let price = document.prfrm.price.value;
     let catname = document.prfrm.catname.value;
@@ -64,14 +69,61 @@ const saveData = ()=>{
     let image = localStorage.getItem('productimage')
     let prod_data = JSON.parse(localStorage.getItem('productInfo'))
     let len = prod_data ? prod_data.length+1 : 1;
-    data.push({
-        pid:len,
-        pname:pname,
-        price:price,
-        category:catname,
-        desc:desc,
-        image:image
-    })
+
+    if(pid != ''){
+        //update
+        let data1 = prod_data.map((i)=>{
+            if(i.pid == pid){
+                i.pname  = pname;
+                i.price  = price;
+                i.category =catname;
+                i.desc = desc;
+                i.image = (image != '')?image:i.image;
+            }
+            return i;
+        })
+        data = data1;
+    } else {
+        //insert
+        data.push({
+            pid:len,
+            pname:pname,
+            price:price,
+            category:catname,
+            desc:desc,
+            image:image
+        })
+    }
+   
     localStorage.setItem("productInfo",JSON.stringify(data))
+    localStorage.removeItem('productimage')
+    document.prfrm.reset()
+    $("#imgthumbnail").attr('src', '');
     dispData();
+}
+
+const editData = (pid)=>{
+    let data = JSON.parse(localStorage.getItem('productInfo'));
+    let data1 = data.find((i)=>{
+                    return i.pid == pid
+                })
+    document.prfrm.prname.value =data1.pname;
+    document.prfrm.price.value = data1.price;
+    document.prfrm.catname.value = data1.category;
+    document.prfrm.desc.value = data1.desc;
+    document.getElementById('imgthumbnail').src = data1.image;
+    document.getElementById('pid').value = pid;   
+}
+const deleteData = (id)=>{
+    let data1 = JSON.parse(localStorage.getItem('productInfo'))
+    data1.splice(id-1,1);
+    console.log(data1);
+    let j=1;
+    data1 = data1.map((i)=>{
+            i.id = j++;
+            return i;
+    })
+    data = data1;
+    localStorage.setItem('productInfo',JSON.stringify(data))
+    dispData()
 }
